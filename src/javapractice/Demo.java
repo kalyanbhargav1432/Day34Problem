8bd5db3b6b9dafa6a606d45daf44d6463f26c0e9
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-11-21 21:56:14 -0800

    Added:Functionality to use database connectivity using payroll_service

--- src/javapractice/Demo.java
+++ src/javapractice/Demo.java
@@ -1,7 +1,38 @@
 package javapractice;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class Demo {
-	public static void main(String args[]) {
-		System.out.println("welcome to jdbc");
+	public static void main(String[] args) {
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
+		String userName = "root";
+		String password = "Kalyan@1432";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in the classpath", e);
+
+		}
+		listDrivers();
+		try {
+			System.out.println("Connection to database:" + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection is successfull !!!!!!!!" + connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println(" " + driverClass.getClass().getName());
+		}
 	}
 }
